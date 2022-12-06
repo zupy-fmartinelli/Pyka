31e86f0664aa092bb920126f9565b0bcc62ad823
Fabio Martinelli <webmaster@zupy.com.br>
2022-12-06 17:21:36 -0300

    Added Delete Form

--- social/views.py
+++ social/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, CommentForm
 
@@ -41,3 +43,19 @@ class PostDetailView(View):
             'form': form,
         }
         return render(request, 'social/post_detail.html', context)
+
+
+class PostEditView(UpdateView):
+    model = Post
+    fields = ['body']
+    template_name = "social/post_edit.html"
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('post-detail', kwargs={'pk': pk})
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "social/post_delete.html"
+    success_url = reverse_lazy('post-list')
