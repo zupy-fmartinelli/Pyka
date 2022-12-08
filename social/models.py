f85258e17ea3741321f9236f5ed116f4801021b4
Fabio Martinelli <webmaster@zupy.com.br>
2022-12-08 16:01:36 -0300

    Profile Picture and Edit Pages

--- social/models.py
+++ social/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -23,6 +25,17 @@ class UserProfile(models.Model):
     name = models.CharField(max_length=30, blank=True, null=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
     birth_date = models.DateField(null=True, blank=True)
-    localtion = models.CharField(max_length=100, blank=True, null=True)
-    picture = models.ImageField(upload_to='uploads/profile_pics', default='uploads/profile_pics/default.png',
+    location = models.CharField(max_length=100, blank=True, null=True)
+    picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/default.png',
                                 blank=True,)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
