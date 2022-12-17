12e799fcbd8a7aac0c894c68b19ce039eaa3e571
Fabio Martinelli <webmaster@zupy.com.br>
2022-12-17 17:40:03 -0300

    Ajustes de CSS

--- setup/settings.py
+++ setup/settings.py
@@ -143,6 +143,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR, "static"
+]
+
 STATIC_URL = "static/"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = '/media/'
